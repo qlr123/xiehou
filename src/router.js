9a4d6ed52c47f9de0557ec32feb53db54de0ac13
wansuiasdn <1003535059@qq.com>
2019-07-18 10:33:07 +0800

    123

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-
+import Login from "./views/myself/login.vue"
 Vue.use(Router);
 
 export default new Router({
@@ -10,7 +10,7 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
-    }
+    },
     // {
     //   path: '/about',
     //   name: 'about',
@@ -19,5 +19,10 @@ export default new Router({
     //   // which is lazy-loaded when the route is visited.
     //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     // }
+    {
+      path:'/login',
+      name:'Login',
+      component:Login
+    }
   ]
 });
---
+++ src/views/myself/login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="login-box">
+        123
+    </div>
+</template>
+<script>
+export default {
+    name:'Login',
+}
+</script>
+<style>
+#login-box{
+    background: linear-gradient(to bottom right, #ffa4d0, #fb7379);
+    width: 100%;
+    height: 100%;
+}
+</style>
