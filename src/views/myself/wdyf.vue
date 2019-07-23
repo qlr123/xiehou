530cb77cebb9a356cbc822532eb0ff31c53e992b
wansuiasdn <1003535059@qq.com>
2019-07-23 21:05:02 +0800

    cnm

--- src/views/myself/wdyf.vue
+++ src/views/myself/wdyf.vue
@@ -19,7 +19,7 @@ export default {
         };
     },
     beforeCreate(){
-        this.$store.commit('lefttitle','返回')//定义导航左侧名字
+        this.$store.commit('lefttitle','返回')//定义导航左侧名字a
         this.$store.commit('righttitle','')//定义导航右侧按钮名字，没功能可以为空
         this.$store.commit('changeTitle','我的缘分')//定义导航中间名字
         this.$store.commit('footercheck','myself')//底部按钮锁定，名字为路由跳转名字
