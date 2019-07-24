c3116cc313e35b78009f6e094d8892ac4d4d5bee
wansuiasdn <1003535059@qq.com>
2019-07-24 17:35:52 +0800

    zjn

--- src/views/myself/wdxx.vue
+++ src/views/myself/wdxx.vue
@@ -1,25 +1,39 @@
 <template>
-    <div>
+    <div id="wdxx">
+        <van-popup v-model="show">
+            <div class="nrbox">
+                <div class="bt"></div>
+                <div class="nr">
+                    <p class="senduser">{{senduser}}:</p>
+                    <p class="content2">{{details.content}}</p>
+                    <p class="time">{{details.sendTime}}</p>
+                </div>
+                <div class="footer">
+                    <van-button plain hairline type="danger" class="btn" @click="reliao">开始热聊</van-button>
+                </div>
+            </div>
+        </van-popup>
         <van-tabs v-model="active">
         <van-tab title="未读消息">
             <div class="wdwd">
                 <div class="title">我的消息</div>
                 <div class="wdnr">
+                    {{neirong1}} 
                     <ul>
-                        <li @click="detail">
-                            <span class="user">螺旋霹雳爆炸</span>
+                        <li @click="detail" v-for="item in xiaoxi1" :key="item.id" :id="item.id">
+                            <span class="user">{{item.username}}</span>
                             <span class="to">给你</span>
                             <span class="do">说</span>
-                            <span class="content">太阳当空照花儿对我笑</span>
-                            <span class="date">2019-7-23</span>
+                            <span class="content">{{item.content}}</span>
+                            <span class="date">{{item.sendTime}}</span>
                         </li> 
-                    </ul>
+                    </ul> 
                 </div>
             </div>
             <div class="xtwd">
                 <div class="title">系统消息</div>
                 <div class="wdnr">
-                    {{neirong}} 
+                    {{neirong2}} 
                 </div>
             </div>
         </van-tab>
@@ -27,13 +41,22 @@
             <div class="wdwd">
                 <div class="title">我的消息</div>
                 <div class="wdnr">
-                    {{neirong}} 
+                    {{neirong3}}
+                    <ul>
+                        <li @click="detail" v-for="item in xiaoxi3" :key="item.id" :id="item.id">
+                            <span class="user">{{item.username}}</span>
+                            <span class="to">给你</span>
+                            <span class="do">说</span>
+                            <span class="content">{{item.content}}</span>
+                            <span class="date">{{item.sendTime}}</span>
+                        </li> 
+                    </ul> 
                 </div>
             </div>
             <div class="xtwd">
                 <div class="title">系统消息</div>
                 <div class="wdnr">
-                    {{neirong}} 
+                    {{neirong4}} 
                 </div>
             </div>
         </van-tab>
@@ -49,12 +72,42 @@ export default {
     data() {
         return {
             active: 2,
-            neirong:'',
+            neirong1:'',
+            xiaoxi1:'',
+            neirong2:'',
+            xiaoxi2:'',
+            neirong3:'',
+            xiaoxi3:'',
+            neirong4:'',
+            xiaoxi4:'',
+            show:false,
+            senduser:'',
+            details:'',
+            userid:''
         };
     },
     methods:{
-        detail(){}
+        detail(data){
+            console.log(data)
+            var name = data.srcElement.parentElement.firstChild.innerHTML;
+            this.userid = data.srcElement.parentElement.id;
+            axios({
+                method:'post',
+                url:'http://10.8.157.63:8080/user/readMessage',
+                data:qs.stringify({id:data.srcElement.parentElement.id}),
+            }).then((data)=>{
+                console.log(data)
+                this.show = true;
+                this.senduser = name
+                this.details = data.data
+                
+         })
+        },
+        reliao(data){
+            this.$router.push({path:'/chat',query:{id:this.userid}})
+        }
     },
+    
     beforeCreate(){
         this.$store.commit('lefttitle','返回')//定义导航左侧名字
         this.$store.commit('righttitle','')//定义导航右侧按钮名字，没功能可以为空
@@ -67,6 +120,14 @@ export default {
             data:qs.stringify({addresser:this.$store.state.userID}),
         }).then((data)=>{
             console.log(data)//未读用户
+            if(data.data.length==0){
+                console.log(123)
+                this.neirong1 = '还没有消息哦'
+            }else{
+                this.neirong1 = ''
+                this.xiaoxi1 = data.data
+            }
+            
         })
 
         axios({
@@ -77,7 +138,10 @@ export default {
             console.log(data)//未读系统
             if(data.data.length==0){
                 console.log(123)
-                this.neirong = '还没有消息哦'
+                this.neirong2 = '还没有消息哦'
+            }else{
+                this.neirong2 = ''
+                this.xiaoxi2 = data.data
             }
         })
 
@@ -87,6 +151,14 @@ export default {
             data:qs.stringify({addresser:this.$store.state.userID}),
         }).then((data)=>{
             console.log(data)//已读用户
+            if(data.data.length==0){
+                console.log(123)
+                this.neirong3 = '还没有消息哦'
+            }else{
+                 console.log(data.data)//已读用户
+                this.neirong3 = ''
+                this.xiaoxi3 = data.data
+            }
         })
 
         axios({
@@ -95,6 +167,13 @@ export default {
             data:qs.stringify({addresser:this.$store.state.userID}),
         }).then((data)=>{
             console.log(data)//已读系统
+            if(data.data.length==0){
+                console.log(123)
+                this.neirong4 = '还没有消息哦'
+            }else{
+                this.neirong4 = ''
+                this.xiaoxi4 = data.data
+            }
         })
     }
 }
@@ -111,7 +190,6 @@ export default {
   border-bottom: 1px solid #eee
 }
 .wdnr{
-    height: 60px;
 }
 .wdnr ul li{
     height: 30px;
@@ -138,5 +216,47 @@ export default {
 .date{
     float: right;
     min-width: 66px;
+}
+.nrbox{
+    width: 80vw;
+    height: 40vh;
+    display: flex;
+    flex-direction: column;
+}
+.bt{
+    width: 100%;
+    height: 60px;
+    background: linear-gradient(to bottom, #fa6481, #fff);
+}
+.nr{
+    flex: 1;
+    padding: 20px;
+}
+.footer{
+    width: 100%;
+    height: 60px;
+    background: linear-gradient(to bottom,#fff,#fa6481);
+}
+.senduser{
+    color: pink;
+    font-size: 16px;
+    text-align: left
+}
+.content2{
+    height: 75%;
+    text-align: left;
+    margin-top: 10px;
+    padding-left: 30px
+}
+.time{
+    text-align: right
+}
+.btn{
+    background: rgba(0, 0, 0, 0);
+    width: 100px;
+    height: 38px;
+    line-height: 38px;
+    margin-top: 10px;
+    color: red
 }
 </style>
